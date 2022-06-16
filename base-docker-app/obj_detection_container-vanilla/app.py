c6b8a620caa4eb87d84812d71499266e3bb6fb6a
Philip Vonderlind <philipv@hotmail.de>
2022-06-16 15:47:33 +0200

    Add basic file uploading REST endpoint and exception handling + file validation.

--- base-docker-app/obj_detection_container-vanilla/app.py
+++ base-docker-app/obj_detection_container-vanilla/app.py
@@ -13,10 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from PIL import Image
-from PIL import ImageDraw
 import os
-import detect
 import tflite_runtime.interpreter as tflite
 import platform
 import datetime
@@ -28,56 +25,54 @@ from io import BytesIO
 from flask import Flask, request, Response, jsonify
 import random
 import re
+import logging
+from datetime import datetime
 
 
 app = Flask(__name__)
 
+def detection_loop(speech_file):
+    pass
 
 
-def detection_loop(filename_image, path, output):
-   pass
-
-
-#initializing the flask app
-app = Flask(__name__)
-
-#routing http posts to this method
+# routing http posts to this method
 @app.route('/api/detect', methods=['POST', 'GET'])
 def main():
-  #img = request.files["image"].read()
-  #image = Image.open(io.BytesIO(img))
-  #data_input = request.args['input']
-  data_input = request.values.get('input')
-  output = request.values.get('output')
-  #output = request.form.get('output')
+    data_input = request.values.get('input')
+    output = request.values.get('output')
+    # output = request.form.get('output')
 
-  path = data_input
-  filename_image = {}
-  
-  input_format = ["jpg", "png", "jpeg"]
-  if data_input.find(".") != -1:
-      print(data_input + " is a file")
-      split_data_input = data_input.split(".", 1)
-      if data_input.endswith(tuple(input_format)):
-          print("INPUT FORMAT: %s IS VALID" % split_data_input[1])
-          path_splitted = []
-          path_splitted = re.split('/', data_input)
-          filename = path_splitted[len(path_splitted)-1]
-          filename_image[filename] = Image.open(data_input)
-          path = os.path.dirname(data_input)+"/"
-  else:
-      print(data_input + " is a path with the following files: ")
-      for filename in os.listdir(data_input):
-          image_path = data_input + filename
-          filename_image[filename] = Image.open(image_path)
-          print("  " + filename)
-  
-  detection_loop(filename_image, path, output)
-  
-  status_code = Response(status = 200)
-  return status_code
+    path = data_input
+    filename_image = {}
+
+    accepted_formats = ["wav"]
+
+    if not request.content_type.split(';')[0] == 'multipart/form-data':
+        return Response(status=415)
+
+    try:
+        speech_file = get_file_from_request(accepted_formats)
+    except KeyError:
+        return Response(status=400)
+    except AssertionError:
+        return Response(status=400)
+
+    detection_loop(speech_file)
+
+    status_code = Response(status=200)
+    return status_code
+
+
 # image=cv2.imread(args.input)
 # image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
 
+def get_file_from_request(accepted_types, file_name="file"):
+    speech_file = request.files[file_name]
+    filetype = speech_file.filename.split('.')[-1]
+    if filetype not in accepted_types:
+        raise AssertionError
+    return speech_file
+
+
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
